2e172d72f3dead00d002a32cd431e8e7689424b6
1227JL <lopezvidaljuliandavid@gmail.com>
2023-04-23 21:47:08 -0500

    Backup

--- Nequi/src/components/AB.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AB = () => {
-  return (
-    <div>AB</div>
-  )
-}
-
-export default AB
--- criptos-react/src/components/Formulario.jsx
+++ criptos-react/src/components/Formulario.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
-
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover{
         background-color: #7A7DFE;
@@ -22,10 +24,37 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
+    const [criptos, setCriptos] = useState([])
+
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+    const [criptomonedas, SelectCriptos] = useSelectMonedas('Elige tu Criptomoneda', criptos)
+
+    useEffect(()=>{
+        const consultarApi = async()=>{
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=500&tsym=USD'
+            
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+            // console.log(resultado.Data);
+
+            const arrayCriptos = resultado.Data.map(cripto => {
+                
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto
+            })
+
+            setCriptos(arrayCriptos)
+        }
+        consultarApi();
+    },[])
 
   return (
     <form action="">
-
+        <SelectMonedas/>
+        <SelectCriptos/>
         <InputSubmit
             type="submit" value='cotizar'
         />
